98ab6f305d043f47709bb114ede36ad52da31734
Eyegor2732 <kovalsky.family@yahoo.com>
2024-07-04 16:21:09 -0400

    Initial commit

---
+++ palyndrome.py
@@ -0,0 +1,14 @@
+def reverse_phrase(some_string):
+    strx = some_string.split()
+    strx.reverse()
+
+    str_output = ""
+    for item in strx:
+        str_output = str_output + " " + item
+
+    return str_output.strip()git
+
+
+mystring = "I'd like to say hello to the whole world!"
+
+print(reverse_phrase(mystring))
